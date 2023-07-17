68b1e01ae0551c42d33de659e6becb5e5047f030
keynes1234 <keynes@naver.com>
2023-07-17 11:55:26 +0900

    0717

---
+++ src/main/java/com/iu/main/api/lang/string/StringMain.java
@@ -0,0 +1,34 @@
+package com.iu.main.api.lang.string;
+
+import java.util.Scanner;
+
+public class StringMain {
+	public static void main(String[] args) {
+		
+		StringStudy stringStudy = new StringStudy();
+		stringStudy.ex2();
+//		StringStudy stringStudy
+		
+//		Scanner sc = new Scanner(System.in);
+//				
+//		String name = "winter";
+//		String name2 = "카리나";
+//		//맴버메서드 호출 문법
+//		//참조변수명.매서드명()
+//		String result = name.concat(name2);
+//		System.out.println(result);
+//		
+//		name2="winter";
+//		
+//		System.out.println(name==name2);
+//		System.out.println("이름 입력");
+//		String name3 = sc.next();
+//		System.out.println(name==name3);
+//		
+//		System.out.println(name.equals(result));
+		
+		
+	}
+	
+	
+}
